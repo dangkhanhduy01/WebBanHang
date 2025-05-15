6fdb8a3092e540b58fd9ba6367820fef88127575
ADMIN <ADMIN@PC218>
2025-05-15 17:00:56 +0700

    web

--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -53,7 +53,7 @@ namespace WebBanHang
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }

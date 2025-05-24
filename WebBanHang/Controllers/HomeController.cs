326cff6cc97592b1e03d06ddc0d813a85fb2e4be
ADMIN <ADMIN@PC329>
2025-05-24 13:48:04 +0700

    lab04

--- WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WebBanHang.Models;
 
 namespace WebBanHang.Controllers
@@ -12,15 +12,26 @@ namespace WebBanHang.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            var pageSize = 3;
+            var dsSanPham = _db.Products.ToList();
+            return View(dsSanPham.Skip((page - 1) * pageSize).Take(pageSize).ToList());
+        }
+
+        public IActionResult LoadMore(int page = 1)
+        {
+            var pageSize = 3;
+            var dsSanPham = _db.Products.ToList();
+            return PartialView("_ProductPartial", dsSanPham.Skip((page - 1) * pageSize).Take(pageSize).ToList());
         }
 
         public IActionResult Privacy()
